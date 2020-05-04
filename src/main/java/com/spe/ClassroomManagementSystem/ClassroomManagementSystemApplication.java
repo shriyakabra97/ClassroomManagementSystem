0f43e24b393eae8cec1e874b957dfc2a90dd61d4
Shriya Kabra <shriyakabra97@gmail.com>
2020-05-04 22:19:52 +0530

    Initial commit

---
+++ src/main/java/com/spe/ClassroomManagementSystem/ClassroomManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.spe.ClassroomManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spe/ClassroomManagementSystem/ClassroomManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spe.ClassroomManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
