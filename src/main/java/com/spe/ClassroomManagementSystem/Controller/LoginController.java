06173151d4dc1f8595c1d875698ce31f2aca9968
mkd1997 <manavdesai1997@gmail.com>
2020-05-05 19:10:46 +0530

    tests written

--- src/main/java/com/spe/ClassroomManagementSystem/Controller/LoginController.java
+++ src/main/java/com/spe/ClassroomManagementSystem/Controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.spe.ClassroomManagementSystem.Controller;
 
 import com.spe.ClassroomManagementSystem.Service.LoginService;
+import com.spe.ClassroomManagementSystem.Service.LoginServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
