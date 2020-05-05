e7f33d48c5d093dfd816eabe5ed475b8757d6f91
mkd1997 <manavdesai1997@gmail.com>
2020-05-05 22:27:03 +0530

    working code

--- src/main/java/com/spe/ClassroomManagementSystem/Controller/LoginController.java
+++ src/main/java/com/spe/ClassroomManagementSystem/Controller/LoginController.java
@@ -3,36 +3,43 @@ package com.spe.ClassroomManagementSystem.Controller;
 import com.spe.ClassroomManagementSystem.Service.LoginService;
 import com.spe.ClassroomManagementSystem.Service.LoginServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class LoginController {
     @Autowired
     private LoginService loginService;
 
-    @GetMapping("/login")
-    public ModelAndView logIntoSystem(@RequestParam String username, @RequestParam String password, @RequestParam String usertype) {
+    @RequestMapping("/login")
+    public RedirectView logIntoSystem(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("usertype") String usertype) {
         boolean loginSuccess = loginService.checkCredentials(username, password, usertype);
-        ModelAndView mv = new ModelAndView();
-        if(loginSuccess == false) {
-            mv.setViewName("index.html");
+//        ModelAndView mv = new ModelAndView();
+//        if(loginSuccess == false) {
+//            mv.setViewName("index.html");
+//        } else {
+//            switch (usertype) {
+//                case "admin": mv.setViewName("AdminDashboard.jsp");
+//                    break;
+//                case "professor": mv.setViewName("ProfessorDashboard.jsp");
+//                    break;
+//                case "ta": mv.setViewName("TADashboard.jsp");
+//                    break;
+//                case "committee": mv.setViewName("CommitteeDashboard.jsp");
+//                    break;
+//                case "sac": mv.setViewName("SACDashboard.jsp");
+//                    break;
+//            }
+//        }
+//        return mv;
+        RedirectView rv = new RedirectView();
+        if (loginSuccess) {
+            rv.setUrl("/AdminDashboard.jsp");
         } else {
-            switch (usertype) {
-                case "admin": mv.setViewName("AdminDashboard.jsp");
-                    break;
-                case "professor": mv.setViewName("ProfessorDashboard.jsp");
-                    break;
-                case "ta": mv.setViewName("TADashboard.jsp");
-                    break;
-                case "committee": mv.setViewName("CommitteeDashboard.jsp");
-                    break;
-                case "sac": mv.setViewName("SACDashboard.jsp");
-                    break;
-            }
+            rv.setUrl("/index.html");
         }
-        return mv;
+
+        return rv;
     }
 }
