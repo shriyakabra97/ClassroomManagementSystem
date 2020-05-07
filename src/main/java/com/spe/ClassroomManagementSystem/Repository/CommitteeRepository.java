b6f8fb9138f06354e81725312b72d331e463981c
pilaniya <pilaniyamukesh0@gmail.com>
2020-05-07 18:20:56 +0530

    Merge branch 'master' into pilaniya
    # Conflicts:
    #	src/main/java/com/spe/ClassroomManagementSystem/Repository/CommitteeRepository.java
    #	src/main/java/com/spe/ClassroomManagementSystem/Repository/LoginRepository.java
    #	src/main/java/com/spe/ClassroomManagementSystem/Repository/ProfessorRepository.java
    #	src/main/java/com/spe/ClassroomManagementSystem/Repository/SacRepository.java

--- src/main/java/com/spe/ClassroomManagementSystem/Repository/CommitteeRepository.java
+++ src/main/java/com/spe/ClassroomManagementSystem/Repository/CommitteeRepository.java
@@ -1,10 +1,7 @@
 package com.spe.ClassroomManagementSystem.Repository;
 
-<<<<<<< HEAD
 import com.spe.ClassroomManagementSystem.Model.Committee;
-=======
-import com.spe.ClassroomManagementSystem.Models.Committee;
->>>>>>> origin/master
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommitteeRepository extends JpaRepository<Committee,Long> {
--- src/main/java/com/spe/ClassroomManagementSystem/Repository/TeachingAssistantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spe.ClassroomManagementSystem.Repository;
-
-import com.spe.ClassroomManagementSystem.Model.TeachingAssistant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeachingAssistantRepository extends JpaRepository<TeachingAssistant,Long> {
-}
