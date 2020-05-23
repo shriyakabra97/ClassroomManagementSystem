af9373d39518477eb2982222a04b2aa44e2b8595
Shriya Kabra <shriyakabra97@gmail.com>
2020-05-23 15:24:18 +0530

    added tests

--- src/test/java/com/spe/ClassroomManagementSystem/Service/ClassroomServiceImplTest.java
+++ src/test/java/com/spe/ClassroomManagementSystem/Service/ClassroomServiceImplTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -43,5 +44,38 @@ public class ClassroomServiceImplTest {
                 new Classroom("A104", 100, true, 100)
         ).collect((Collectors.toList())));
         Assert.assertEquals(2, classroomService.getClassroomByFormFilter(capacity, plugs, projectorAvailable).size());
+    }
+
+    @Test
+    public void findByClassCode(){
+        String classCode= "A102";
+        when(classroomRepository.findByClassCode(classCode)).thenReturn(new Classroom("A102", 50, true, 0));
+
+//        when(classroomRepository.findByClassCode(classCode)).thenReturn(Stream.of(
+//                new Classroom("A102", 50, true, 0)
+//        ).collect((Collectors.toList())));
+        Assert.assertEquals(classCode, classroomService.findByClassCode(classCode).getClassCode());
+
+    }
+    @Test
+    public void saveClassroom(){
+        Classroom classroom = new Classroom("A102", 50, true, 0);
+        when(classroomRepository.save(classroom)).thenReturn(
+                new Classroom("A102", 50, true, 0)
+        );
+        Assert.assertEquals("Saved Classroom Successfully", classroomService.saveClassroom(classroom));
+    }
+
+    @Test
+    public void getClassroomByFormFilterWithoutProjectorConstraint(){
+        long capacity = 50;
+        long plugs = 0 ;
+        when(classroomRepository.findAllByCapacityGreaterThanEqualAndPlugsGreaterThanEqual(capacity, plugs)).thenReturn((List<Classroom>) Stream.of(
+                new Classroom("A102", 50, false, 10),
+                new Classroom("A103", 50, true, 0),
+                new Classroom("A104", 100, true, 100),
+                new Classroom("A105", 200, false, 20)
+        ).collect((Collectors.toList())));
+        Assert.assertEquals(4, classroomService.getClassroomByFormFilterWithoutProjectorConstraint(capacity, plugs).size());
     }
 }
